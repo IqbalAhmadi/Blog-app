3db8ad2af4d896146384d70e76aa4ea7ebb4bd84
Mohammad Ahmadi <iqbal_dyncorp@live.com>
2024-12-08 16:57:16 -0800

    installed required packages, created pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,47 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from 'react-router-dom'
+import Homepage from './routes/Homepage.jsx'
+import PostListPage from './routes/PostListPage.jsx'
+import Write from './routes/Write.jsx'
+import LoginPage from './routes/LoginPage.jsx'
+import RegisterPage from './routes/RegisterPage.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Homepage/>
+  },
+  {
+    path: '/posts',
+    element: <PostListPage/>
+  },
+  {
+    path: '/:slug',
+    element: <PostListPage/>
+  },
+  {
+    path: '/write',
+    element: <Write/>
+  },
+  {
+    path: '/login',
+    element: <LoginPage/>
+  },
+  {
+    path: '/:slug',
+    element: <RegisterPage/>
+  },
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
